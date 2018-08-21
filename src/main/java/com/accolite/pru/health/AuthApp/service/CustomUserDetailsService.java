73ccf3a62092258e6612dd35b8726bb0981342ab
Aman Garg <amangargcse@outlook.com>
2018-08-21 16:52:19 +0530

    Add loadById in userRepository

--- src/main/java/com/accolite/pru/health/AuthApp/service/CustomUserDetailsService.java
+++ src/main/java/com/accolite/pru/health/AuthApp/service/CustomUserDetailsService.java
@@ -1,14 +1,15 @@
 package com.accolite.pru.health.AuthApp.service;
 
-import com.accolite.pru.health.AuthApp.exception.UserAuthenticationException;
 import com.accolite.pru.health.AuthApp.model.CustomUserDetails;
 import com.accolite.pru.health.AuthApp.model.User;
 import com.accolite.pru.health.AuthApp.repository.UserRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,12 +19,24 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private Logger logger;
+
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<User> dbUser = userRepository.findByEmail(email);
-		System.out.println("Got user: " + dbUser + " for " + email);
+		logger.info("Got user: " + dbUser + " for " + email);
 		return dbUser.map(CustomUserDetails::new)
-				.orElseThrow(() -> new UserAuthenticationException("Couldn't find a matching email in the " +
+				.orElseThrow(() -> new UsernameNotFoundException("Couldn't find a matching email in the " +
 						"database for " + email));
+	}
+
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		Optional<User> dbUser = userRepository.findById(id);
+		logger.info("Got user: " + dbUser + " for " + id);
+		return dbUser.map(CustomUserDetails::new)
+				.orElseThrow(() -> new UsernameNotFoundException("Couldn't find a matching id in the " +
+						"database for " + id));
 	}
 }
