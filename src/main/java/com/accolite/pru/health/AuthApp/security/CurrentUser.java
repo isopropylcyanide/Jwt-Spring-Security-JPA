7a18aafb40ea8b4d6651aabbd689c17541652100
Aman Garg <amangargcse@outlook.com>
2018-08-21 18:43:02 +0530

    Add currentUser annotation

---
+++ src/main/java/com/accolite/pru/health/AuthApp/security/CurrentUser.java
@@ -0,0 +1,20 @@
+package com.accolite.pru.health.AuthApp.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Custom annotation to access currently authenticated user in the codebase. Works the same as
+ * AuthenticationPrincipal. Created to reduce dependency on Spring Security
+ */
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
