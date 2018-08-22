2cf94379525d36aeb7a9ad950cb84cebfb47d9e9
Aman Garg <amangargcse@outlook.com>
2018-08-22 16:19:01 +0530

    Inject & test current user

--- src/main/java/com/accolite/pru/health/AuthApp/controller/UserController.java
+++ src/main/java/com/accolite/pru/health/AuthApp/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.accolite.pru.health.AuthApp.controller;
 
+import com.accolite.pru.health.AuthApp.model.CustomUserDetails;
 import com.accolite.pru.health.AuthApp.model.payload.ApiResponse;
+import com.accolite.pru.health.AuthApp.security.CurrentUser;
 import com.accolite.pru.health.AuthApp.service.AuthService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,14 +35,15 @@ public class UserController {
 	}
 
 	@GetMapping("/me")
-	@Secured("ROLE_USER")
-	public ResponseEntity<?> getUserProfile() {
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<?> getUserProfile(@CurrentUser CustomUserDetails customUserDetails) {
 		logger.info("Inside secured resource with user");
+		logger.info(customUserDetails.getEmail() + " has role: " + customUserDetails.getRoles());
 		return ResponseEntity.ok("Hello. This is about me");
 	}
 
 	@GetMapping("/admins")
-	@Secured("ROLE_ADMIN")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<?> sayAlternateHello() {
 		logger.info("Inside secured resource with admin");
 		return ResponseEntity.ok("Hello. This is about admins");
