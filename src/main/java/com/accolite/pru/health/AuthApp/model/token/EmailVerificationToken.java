939c9f3b54c334cf1c2f76dae234e6e15395e1aa
Aman Garg <amangargcse@outlook.com>
2018-08-24 12:29:27 +0530

    Add EmailVerificationToken and move token to a separate package

---
+++ src/main/java/com/accolite/pru/health/AuthApp/model/token/EmailVerificationToken.java
@@ -0,0 +1,79 @@
+package com.accolite.pru.health.AuthApp.model.token;
+
+import com.accolite.pru.health.AuthApp.model.User;
+import com.accolite.pru.health.AuthApp.model.audit.DateAudit;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import java.time.Instant;
+
+@Entity(name = "EMAIL_VERIFICATION_TOKEN")
+public class EmailVerificationToken extends DateAudit {
+
+	@Id
+	@Column(name = "TOKEN_ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "email_token_seq")
+	@SequenceGenerator(name = "email_token_seq", allocationSize = 1)
+	private Long id;
+
+	@NaturalId
+	@Column(name = "TOKEN", nullable = false, unique = true)
+	private String token;
+
+	@OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+	@JoinColumn(nullable = false, name = "USER_ID")
+	private User user;
+
+	@Column(name = "EXPIRY_DT", nullable = false)
+	private Instant expiryDate;
+
+	public EmailVerificationToken() {
+	}
+
+	public EmailVerificationToken(Long id, String token, User user, Instant expiryDate) {
+		this.id = id;
+		this.token = token;
+		this.user = user;
+		this.expiryDate = expiryDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Instant getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(Instant expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+}
