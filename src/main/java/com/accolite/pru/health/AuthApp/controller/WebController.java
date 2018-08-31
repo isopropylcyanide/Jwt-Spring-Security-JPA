16dc8d89fb102430e7aa166d82144706f10369d5
Aman Garg <amangargcse@outlook.com>
2018-08-31 15:53:49 +0530

    Remove unnecessary api

--- src/main/java/com/accolite/pru/health/AuthApp/controller/WebController.java
+++ src/main/java/com/accolite/pru/health/AuthApp/controller/WebController.java
@@ -1,19 +1,18 @@
 package com.accolite.pru.health.AuthApp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 
-	@GetMapping("/api/auth/password/reset")
-	public String resetPassword(@RequestParam(value = "token") String token) {
+	@GetMapping("/password/reset")
+	public String resetPassword(@RequestParam(value = "token") String token, ModelMap model) {
+		String url = "/api/auth/password/reset?token=" + token;
+		model.addAttribute("url", url);
 		return "reset-password";
 	}
 
-	@GetMapping("/api/auth/password/reset/re")
-	public String resetPassword() {
-		return "reset-password1";
-	}
 }
