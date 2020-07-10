cd9777c01265b6c9b5415935f897401d73ec73da
Mahdi Jaberzadeh Ansari <mahdijaberzadehansari@yahoo.co.uk>
2020-07-10 19:39:43 +0200

    More static file types added  (#21)
    * fixed the missing this pointers in DeviceInfo
    
    * Added more file types as static files without auth

--- src/main/java/com/accolite/pru/health/AuthApp/config/WebSecurityConfig.java
+++ src/main/java/com/accolite/pru/health/AuthApp/config/WebSecurityConfig.java
@@ -86,11 +86,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/",
-                        "/favicon.ico",
+                		"/favicon.ico",
+                        "/**/*.json",
+                        "/**/*.xml",
+                        "/**/*.properties",
+                        "/**/*.woff2",
+                        "/**/*.woff",
+                        "/**/*.ttf",
+                        "/**/*.ttc",
+                        "/**/*.ico",
+                        "/**/*.bmp",
                         "/**/*.png",
                         "/**/*.gif",
                         "/**/*.svg",
                         "/**/*.jpg",
+                        "/**/*.jpeg",
                         "/**/*.html",
                         "/**/*.css",
                         "/**/*.js").permitAll()
