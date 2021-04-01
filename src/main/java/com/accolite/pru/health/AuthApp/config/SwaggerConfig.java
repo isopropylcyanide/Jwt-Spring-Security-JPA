8b12255f391699ba7302c7f74742aa39d077c0d2
krupal <krupalshah55@gmail.com>
2021-04-01 12:40:34 +0530

    Update SwaggerConfig.java

--- src/main/java/com/accolite/pru/health/AuthApp/config/SwaggerConfig.java
+++ src/main/java/com/accolite/pru/health/AuthApp/config/SwaggerConfig.java
@@ -22,6 +22,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.accolite.pru.health.AuthApp.annotation.CurrentUser;
 
 @EnableSwagger2
 @Configuration
